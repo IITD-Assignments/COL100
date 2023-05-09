b422bc23b245f65aa3d70e61331b892bfabf23e5
Harsha Vardhan <x.projekt@outlook.com>
2023-05-09 22:05:03 +0530

    feat: create f372.py

--- f372.py
+++ f372.py
@@ -1,2 +1,11 @@
 def print_pattern(n):
-    pass
+    while True:
+        if n < 5:
+            for i in range(1, n):
+                for j in range(1, i+1):
+                    if i == j:
+                        return i
+                        break
+                    elif i < j:
+                        return i, i-1
+                        break
