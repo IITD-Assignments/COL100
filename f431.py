fae65d44b69ce5f02d3811f24486d71aa4baa364
Harsha Vardhan <x.projekt@outlook.com>
2023-05-10 19:11:26 +0530

    feat: create f431.py

---
+++ f431.py
@@ -0,0 +1,11 @@
+def print_pattern(n):
+    k = n
+    s = str(n)
+    L = range(1, n-1)
+    for a in range(n):
+        print(" " * (k-1), s)
+        s = ""
+        for b in range(-1, -a-2, -1):
+            s += str(L[b])
+        s = s[:] + s[-2::-1]
+        k-=1
