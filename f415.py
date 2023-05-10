33b92f7f7bc3d98c5d3069b66b603022804dfb70
Harsha Vardhan <x.projekt@outlook.com>
2023-05-10 18:09:31 +0530

    feat: create f415.py

---
+++ f415.py
@@ -0,0 +1,13 @@
+def print_pattern(n):
+    while i <= n:
+    j = n - 1
+    while j >= 1:
+        print(" ", 'ent')
+        j = j - 1
+    m = n
+    for k in range(1, i+1):
+        print(" ", 'ent')
+        m = n +1
+    for k in range(2, i+1):
+        print(" ", 'ent')
+        m = n + 2
