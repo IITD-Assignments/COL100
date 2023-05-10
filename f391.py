816fae9bf9a82420c02c3e2d8d4d0d29c826bb22
Harsha Vardhan <x.projekt@outlook.com>
2023-05-10 16:24:56 +0530

    feat: create f391.py

---
+++ f391.py
@@ -0,0 +1,10 @@
+def print_pattern(n):
+    for i in range(n+1):
+        j = n
+        while j > 0:
+            print(" ", end=" ")
+            j = n- 1
+        while k > 0:
+            print("k", end="")
+            k=k+2
+        print()
