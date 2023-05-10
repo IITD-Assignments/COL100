875a92061cef9a865929ea6195c10ee110e16a6e
Harsha Vardhan <x.projekt@outlook.com>
2023-05-10 16:38:34 +0530

    feat: create f396.py

---
+++ f396.py
@@ -0,0 +1,16 @@
+def print_pattern(n):
+    i = 1
+    j = n
+    while (i <= n):
+        j = n
+        while (j >= i):
+            print(j, end="")
+            j -=1
+        
+        j = i + 1
+        while (j <= n):
+            print(j, end="")
+            j += 1
+        
+        i+= 1
+        print("")
