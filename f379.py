1698233a3fbb0e44b8faca46494725f3a8aa0617
Harsha Vardhan <x.projekt@outlook.com>
2023-05-17 16:18:44 +0530

    fix: illegible hand-writing

--- f379.py
+++ f379.py
@@ -5,5 +5,5 @@ def print_pattern(n):
         for j in range(n-i):
             print(n-j, end="")
         for k in range(2*n-1-n-i):
-            print(n-i+k+k, end="")
+            print(n-j+1+k, end="")
         print()
